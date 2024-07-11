664d26dd4fd85f67c9b1846cf7d88b028a0974a2
Paul Keen <125715+pftg@users.noreply.github.com>
2024-07-11 16:27:27 +0200

    feat: show details of the current step

--- src/App.vue
+++ src/App.vue
@@ -94,7 +94,7 @@
           <div class="mt-3 space-y-1">
             <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href"
               class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">{{
-              item.name }}</DisclosureButton>
+                item.name }}</DisclosureButton>
           </div>
         </div>
       </DisclosurePanel>
@@ -103,12 +103,12 @@
     <div class="py-10">
       <header>
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Dashboard</h1>
+          <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Engineering Manager</h1>
         </div>
       </header>
       <main>
         <div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-          <!-- Your content -->
+          <ThePlaybook />
         </div>
       </main>
     </div>
@@ -118,6 +118,7 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import ThePlaybook from './components/ThePlaybook.vue'
 
 const user = {
   name: 'Tom Cook',
---
+++ src/components/ThePlaybook.vue
@@ -0,0 +1,65 @@
+<template>
+  <ul role="list" class="divide-y divide-gray-100">
+    <li v-for="track in tracks" :key="track.id"
+      class="relative flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap">
+      <div>
+        <p class="text-sm font-semibold leading-6 text-gray-900">
+          <a @click.prevent="showDrawer(track)" href="#" class="hover:underline">
+            <span class="absolute inset-x-0 -top-px bottom-0" />
+            {{ track.title }}
+          </a>
+        </p>
+        <div class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+          <p>
+            <a :href="track.author.href" class="hover:underline">{{ track.author.name }}</a>
+          </p>
+          <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
+            <circle cx="1" cy="1" r="1" />
+          </svg>
+          <p>
+            <time :datetime="track.dateTime">{{ track.date }}</time>
+          </p>
+        </div>
+      </div>
+      <dl class="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
+        <ChevronRightIcon class="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+      </dl>
+    </li>
+  </ul>
+  <Step :title="selectedTrack.title" :details="selectedTrack.details" v-if="selectedTrack" />
+</template>
+
+<script setup>
+import { ChatBubbleLeftIcon, CheckCircleIcon, ChevronRightIcon } from '@heroicons/vue/24/outline'
+import Step from './Step.vue'
+import { ref } from 'vue'
+
+const tracks = ref([
+  {
+    id: 1,
+    title: 'Atque perspiciatis et et aut ut porro voluptatem blanditiis?',
+    details: 'Description 1',
+    href: '#',
+    author: { name: 'Tekkr', href: '#' },
+    date: '2d ago',
+    dateTime: '2023-01-23T22:34Z',
+    status: 'active',
+  },
+  {
+    id: 2,
+    title: 'Et ratione distinctio nesciunt recusandae vel ab?',
+    details: 'Description 2',
+    href: '#',
+    author: { name: 'Tekkr', href: '#' },
+    date: '2d ago',
+    dateTime: '2023-01-23T19:20Z',
+    status: 'active'
+  }
+])
+
+const selectedTrack = ref(null)
+
+const showDrawer = (track) => {
+  selectedTrack.value = track
+}
+</script>
