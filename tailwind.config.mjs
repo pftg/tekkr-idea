9895b5cfa8af6944185787a39560dbd47d92754c
Paul Keen <125715+pftg@users.noreply.github.com>
2024-07-11 14:59:59 +0200

    build: setup tailwindcss to watch index.html

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, html,js}"
+  ],
   theme: {
     extend: {},
   },
